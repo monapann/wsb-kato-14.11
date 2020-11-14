d95ddccf92acc5dc18d030eed604a87b53dca03b
Marcin Dlubis <marcindl@hotmail.com>
2020-11-14 09:00:51 +0100

    objects - introduction

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/////////// Obiekty ////////////
+
+const firstNameEmployee = "Marcin";
+const familyNameEmployee = "Dłubis";
+const ageEmployee = "32";
+const occupationEmployee = "dev";
+
+const employee = {
+    firstName: "Marcin",
+    familyName: "Dłubis",
+    age: 32,
+    occupation: "dev"
+}
