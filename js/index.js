56fd4a543f3b80f981622a09e68bee9321bc6e88
Marcin Dlubis <marcindl@hotmail.com>
2020-11-14 09:59:20 +0100

    objects

--- js/index.js
+++ js/index.js
@@ -19,3 +19,27 @@ const employee = {
 console.log(employee.introduction())
 // console.log(employee["start salary"])
 
+
+//Zadanie 1
+// Utwórz funkcję, która przyjmuje rozmiary argumentów obiektu (zawiera klucze szerokości, długości, wysokości) i zwraca objętość pudełka.
+
+// volumeOfBox({ width: 2, length: 5, height: 1 }) ➞ 10
+// volumeOfBox({ width: 4, length: 2, height: 2 }) ➞ 16
+// volumeOfBox({ width: 2, length: 3, height: 5 }) ➞ 30
+
+const boxSizes = {
+    width: 10,
+    height: 20,
+    length: 10
+}
+
+// function volumeOfBox(box){
+//     // console.log(box.height)
+//     return `Objętość pudełka wynosi: ${box.height * box.width * box.length}`
+// }
+
+// console.log(volumeOfBox(boxSizes))
+
+const volumeOfBox = box => `Objętość pudełka wynosi: ${box.height * box.width * box.length}`
+
+console.log(volumeOfBox(boxSizes))
