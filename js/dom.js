4019cba30495936e6243803ff42e037f8cb4e483
Marcin Dlubis <marcindl@hotmail.com>
2020-11-14 12:55:18 +0100

    dom

--- js/dom.js
+++ js/dom.js
@@ -4,4 +4,10 @@ const identifyID_2 = document.querySelector("#sample");
 console.log(identifyID_2);
 const identifyID_3 = document.querySelector(".sampleCl");
 console.log(identifyID_3);
+const identifyID_4 = document.querySelector("a");
+console.log(identifyID_4);
+const identifyID_5 = document.querySelectorAll("a");
+console.log(identifyID_5);
+const identifyID_6 = document.getElementsByTagName("a");
+console.log(identifyID_6);
 
