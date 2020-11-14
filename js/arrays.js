f0f6306d194619bc89ffc6929fe1b7174d41147f
Marcin Dlubis <marcindl@hotmail.com>
2020-11-14 11:12:25 +0100

    arrays

---
+++ js/arrays.js
@@ -0,0 +1,10 @@
+const obj = {
+    country: "Poland",
+    city: "Katowice"
+}
+
+const arr = [1,"Marcin",2,{name:"Marcin",family:"yes"}, obj];
+console.log(arr[4].country);
+console.log(arr[0]=3);
+console.log(arr);
+
