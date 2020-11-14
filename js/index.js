70e08580a81ffb42f3947d52098f495243db2b56
Marcin Dlubis <marcindl@hotmail.com>
2020-11-14 09:25:20 +0100

    objects

--- js/index.js
+++ js/index.js
@@ -9,7 +9,13 @@ const employee = {
     firstName: "Marcin",
     familyName: "Dłubis",
     age: 32,
-    occupation: "dev"
+    occupation: "dev",
+    "startSalary": 3200,
+    introduction: function() {
+        // return "Pracownik " + employee.firstName
+        return `Pracownik ${this.firstName} ${this.familyName}, w wieku ${this.age} zatrudniony jest na stanowisku ${this.occupation} i jego pensja początkowa to ${this["start salary"]}`
+    }
 }
+console.log(employee.introduction())
+// console.log(employee["start salary"])
 
-console.log(employee)
